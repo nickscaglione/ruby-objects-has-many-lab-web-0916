d29ff03d7bff5c52b1953f2e27acd578b972494a
nickscaglione <nicholas.a.scaglione@gmail.com>
2016-09-30 15:34:46 -0400

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,29 @@
+class Artist
+
+  attr_accessor :name, :songs
+
+  @@song_count = 0
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def add_song(song)
+    songs << song
+    song.artist = self
+    @@song_count += 1
+  end
+
+  def add_song_by_name(song_name)
+    songs << Song.new(song_name)
+    songs.last.name = song_name
+    songs.last.artist = self
+    @@song_count += 1
+  end
+
+  def self.song_count
+    @@song_count
+  end
+
+end
